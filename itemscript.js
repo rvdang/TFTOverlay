f583cfff593328bcf3581935b5701b77d544f655
alexyip999 <35481543+alexyip999@users.noreply.github.com>
2019-07-24 00:41:31 -0700

    fix increment/decrement

--- itemscript.js
+++ itemscript.js
@@ -82,25 +82,12 @@ function reset(currInv, allitems) {
 // takes in an item index to increment, your current 
 // inventory (1x8 array of half-items) and the 8x8 table of 
 // craftable items and updates your inventory and table accordingly
-// takes in your current inventory (1x8 array of half-items) 
-// and the 8x8 table of craftable items and sets their contents
-// to 0, returning a list containing both objects
-function reset(currInv, allitems) {
-  currInv = new Array(8).fill(0);
-  allitems = Array.from(Array(8), _ => Array(8).fill(0));
-  return [currInv, allitems];
-}
 
-// takes in an item index to increment, your current 
-// inventory (1x8 array of half-items) and the 8x8 table of 
-// craftable items and updates your inventory and table accordingly
 function incrementtable(item, currInv, compItems) {
   for (let i = 0; i < item; i++) {
     if (currInv[i] == 0) {
-      compItems[item][i] = -1
-    } else if (compItems[item][i] == -1 && currInv[i] >= 1) {
-      compItems[item][i] = 1;
-    } else if (currInv[i] > compItems[item][i]) {
+      compItems[item][i] = -1;
+    } else if (currInv[i] > compItems[i][item]) {
       compItems[item][i] += 1;
     }
   }
@@ -113,14 +100,10 @@ function incrementtable(item, currInv, compItems) {
       } else if (currInv[j]%2 == 1) {
         compItems[j][item] += 1
       }
-    } else {
-      if (currInv[j] == 0) {
-        compItems[j][item] = -1;
-      } else if (compItems[j][item] == -1 && currInv[j] >= 1) {
-        compItems[j][item] = 1;
-      } else if (currInv[j] > compItems[j][item]) {
-        compItems[j][item] += 1;
-      }
+    } else if (currInv[j] == 0) {
+      compItems[j][item] = -1;
+    } else if (currInv[j] > compItems[j][item]) {
+      compItems[j][item] += 1;
     }
   }
   currInv[item] += 1
@@ -129,36 +112,31 @@ function incrementtable(item, currInv, compItems) {
 // takes in an item index to decrement, your current 
 // inventory (1x8 array of half-items) and the 8x8 table of 
 // craftable items and updates your inventory and table accordingly
-function decrementtable(item, currInv, compItems) {
+function decrement(item, currInv, compItems) {
   for (let i = 0; i < item; i++) {
-
-
-
-    if (currInv[item] == 1 && compItems[item][i] == -1) {
-      compItems[item][i] = 0;
-    } else if (currInv[item] == 1 && compItems[item][i] == 1) {
+    if (currInv[item] == 1 && compItems[item][i] == 1) {
       compItems[item][i] = -1;
-    } else if (currInv[item] <= compItems[item][i]) {
+    } else if (currInv[item] == 1 && compItems[item][i] == -1) {
+      compItems[item][i] = 0;
+    } else if (currInv[item] == compItems[item][i]) {
       compItems[item][i] -= 1;
     }
   }
   for (let j = item; j < 8; j++) {
     if (j == item) {
-      if (currInv[item] == 1 && compItems[j][item] == -1) {
+      if (compItems[j][item] == -1 && currInv[item] == 1) {
         compItems[j][item] = 0;
-      } else if (currInv[j] == 2) {
+      } else if (currInv[j] <= 2) {
         compItems[j][item] = -1;
       } else if (currInv[j]%2 == 0) {
         compItems[j][item] -= 1
       }
-    } else {
-      if (currInv[item] == 1 && compItems[j][item] == -1) {
-        compItems[j][item] = 0;
-      } else if (currInv[item] == 1 && compItems[j][item] == 1) {
-        compItems[j][item] = -1;
-      } else if (currInv[item] <= compItems[j][item]) {
-        compItems[j][item] -= 1;
-      }
+    } else if (compItems[j][item] == -1 && currInv[item] == 1) {
+      compItems[j][item] = 0;
+    } else if (currInv[item] == 1) {
+      compItems[j][item] = -1;
+    } else if (currInv[item] == compItems[j][item]) {
+      compItems[j][item] -= 1;
     }
   }
   currInv[item] -= 1
